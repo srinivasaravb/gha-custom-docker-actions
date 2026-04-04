719239d28774a50332091e729aa94d76b7800d4d
Srinivasa Rao B <srenivas35@gmail.com>
2026-04-04 10:32:24 +0530

    added aws secrets

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -5,11 +5,15 @@ from botocore.config import Config
 
 
 def run():
-    bucket = os.environ['INPUT_BUCKET']
-    bucket_region = os.environ['INPUT_REGION']
-    dist_folder = os.environ['INPUT_DIS-FOLDER']
+    aws_access_key = os.getenv("INPUT_AWS_ACCESS_KEY_ID")
+    aws_secret_key = os.getenv("INPUT_AWS_SECRET_ACCESS_KEY")
+    bucket = os.getenv("INPUT_BUCKET")
+    bucket_region = os.getenv("INPUT_REGION")
+    dist_folder = os.getenv("INPUT_DIS-FOLDER")
 
     configuration = Config(region_name=bucket_region)
+    configuration = Config(aws_access_key=aws_access_key)
+    configuration = Config(aws_secret_key=aws_secret_key)
 
     s3_client = boto3.client('s3', config=configuration)
 
